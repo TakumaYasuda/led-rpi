cc30d252810b47bbc08ab5d5c3b6de080a905ebc
mizukimoriya <mimoriya@iyell.jp>
2018-03-13 15:00:10 +0900

    画像修正

--- bindings/python/samples/image-happy.py
+++ bindings/python/samples/image-happy.py
@@ -6,7 +6,7 @@ from PIL import Image
 class ImageScroller(SampleBase):
     def __init__(self, *args, **kwargs):
         super(ImageScroller, self).__init__(*args, **kwargs)
-        self.parser.add_argument("-i", "--image", help="The image to display", default="../../../img/birthday.jpeg")
+        self.parser.add_argument("-i", "--image", help="The image to display", default="../../../img/birthday.png")
 
     def run(self):
         if not 'image' in self.__dict__:
